613a32af7cae109ff25772d1a516d0510dd69916
nn12vl9l <nn12vl9lsoft@gmail.com>
2023-11-04 08:24:49 +0900

    記事更新機能作成

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,6 +6,7 @@
                 {{ session('notice') }}
             </div>
         @endif
+        <x-validation-errors :errors="$errors" />
         <article class="mb-2">
             <h2 class="font-bold font-sans break-normal text-gray-900 pt-6 pb-1 text-3xl md:text-4xl break-words">
                 {{ $post->title }}</h2>
@@ -16,6 +17,9 @@
                 {{ $post->created_at }}
             </p>
             <img src="{{ $post->image_url }}" alt="" class="mb-4">
+            <p class="text-gray-700 text-base">{!! nl2br(e($post->body)) !!}</p>
+        </article>
+        <div class="flex flex-row text-center my-4">
             @can('update', $post)
                 <a href="{{ route('posts.edit', $post) }}"
                     class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-20 mr-2">編集</a>
@@ -28,6 +32,6 @@
                         class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-20">
                 </form>
             @endcan
-    </div>
+        </div>
     </div>
 </x-app-layout>
